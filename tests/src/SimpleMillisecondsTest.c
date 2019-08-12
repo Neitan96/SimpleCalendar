ae157020671d220bd39338223e3bd2c0b57d36bf
Neitan96 <neitan96@outlook.com>
2019-08-11 22:36:14 -0300

    Atualizado testes para novos nomes das funções

--- tests/src/SimpleMillisecondsTest.c
+++ tests/src/SimpleMillisecondsTest.c
@@ -12,190 +12,190 @@
 
 /* Adds */
 
-void scMAddSecondTest(){
+void scMilliAddSecondTest(){
 	setNewTest(__func__, __FILE__);
 
 	//Sep 22 2018 15:22:00
 	Milliseconds milliseconds = 1537629720000;
-	scMAddSecond(&milliseconds, 22);
+	scMilliAddSecond(&milliseconds, 22);
 	scTEqualMilli(__LINE__, 1537629742000, milliseconds);
 
 	//Sep 22 2018 15:22:00
 	milliseconds = 1537629720000;
-	scMAddSecond(&milliseconds, 82);
+	scMilliAddSecond(&milliseconds, 82);
 	scTEqualMilli(__LINE__, 1537629802000, milliseconds);
 
 	//Sep 22 2018 15:22:00
 	milliseconds = 1537629720000;
-	scMAddSecond(&milliseconds, -82);
+	scMilliAddSecond(&milliseconds, -82);
 	scTEqualMilli(__LINE__, 1537629638000, milliseconds);
 
 	//Sep 22 2018 15:22:00
 	milliseconds = 1537629720000;
-	scMAddSecond(&milliseconds, -22);
+	scMilliAddSecond(&milliseconds, -22);
 	scTEqualMilli(__LINE__, 1537629698000, milliseconds);
 
 
 }
 
-void scMAddMinuteTest(){
+void scMilliAddMinuteTest(){
 	setNewTest(__func__, __FILE__);
 
 	//Sep 22 2018 15:22:00
 	Milliseconds milliseconds = 1537629720000;
-	scMAddMinute(&milliseconds, 22);
+	scMilliAddMinute(&milliseconds, 22);
 	scTEqualMilli(__LINE__, 1537631040000, milliseconds);
 
 	//Sep 22 2018 15:22:00
 	milliseconds = 1537629720000;
-	scMAddMinute(&milliseconds, 60);
+	scMilliAddMinute(&milliseconds, 60);
 	scTEqualMilli(__LINE__, 1537633320000, milliseconds);
 
 	//Sep 22 2018 15:22:00
 	milliseconds = 1537629720000;
-	scMAddMinute(&milliseconds, 1462);
+	scMilliAddMinute(&milliseconds, 1462);
 	scTEqualMilli(__LINE__, 1537717440000, milliseconds);
 
 	//Sep 22 2018 15:22:00
 	milliseconds = 1537629720000;
-	scMAddMinute(&milliseconds, -22);
+	scMilliAddMinute(&milliseconds, -22);
 	scTEqualMilli(__LINE__, 1537628400000, milliseconds);
 
 	//Sep 22 2018 15:22:00
 	milliseconds = 1537629720000;
-	scMAddMinute(&milliseconds, -60);
+	scMilliAddMinute(&milliseconds, -60);
 	scTEqualMilli(__LINE__, 1537626120000, milliseconds);
 
 	//Sep 22 2018 15:22:00
 	milliseconds = 1537629720000;
-	scMAddMinute(&milliseconds, -1462);
+	scMilliAddMinute(&milliseconds, -1462);
 	scTEqualMilli(__LINE__, 1537542000000, milliseconds);
 
 }
 
-void scMAddHourTest(){
+void scMilliAddHourTest(){
 	setNewTest(__func__, __FILE__);
 
 	//Sep 22 2018 15:22:00
 	Milliseconds milliseconds = 1537629720000;
-	scMAddHour(&milliseconds, 22);
+	scMilliAddHour(&milliseconds, 22);
 	scTEqualMilli(__LINE__, 1537708920000, milliseconds);
 
 	//Sep 22 2018 15:22:00
 	milliseconds = 1537629720000;
-	scMAddHour(&milliseconds, 3);
+	scMilliAddHour(&milliseconds, 3);
 	scTEqualMilli(__LINE__, 1537640520000, milliseconds);
 
 	//Sep 22 2018 15:22:00
 	milliseconds = 1537629720000;
-	scMAddHour(&milliseconds, 122);
+	scMilliAddHour(&milliseconds, 122);
 	scTEqualMilli(__LINE__, 1538068920000, milliseconds);
 
 	//Sep 22 2018 15:22:00
 	milliseconds = 1537629720000;
-	scMAddHour(&milliseconds, -22);
+	scMilliAddHour(&milliseconds, -22);
 	scTEqualMilli(__LINE__, 1537550520000, milliseconds);
 
 	//Sep 22 2018 15:22:00
 	milliseconds = 1537629720000;
-	scMAddHour(&milliseconds, -3);
+	scMilliAddHour(&milliseconds, -3);
 	scTEqualMilli(__LINE__, 1537618920000, milliseconds);
 
 	//Sep 22 2018 15:22:00
 	milliseconds = 1537629720000;
-	scMAddHour(&milliseconds, -122);
+	scMilliAddHour(&milliseconds, -122);
 	scTEqualMilli(__LINE__, 1537190520000, milliseconds);
 
 }
 
-void scMAddMonthDayTest(){
+void scMilliAddMonthDayTest(){
 	setNewTest(__func__, __FILE__);
 
 	//Sep 22 2018 15:22:00
 	Milliseconds milliseconds = 1537629720000;
-	scMAddMonthDay(&milliseconds, 1);
+	scMilliAddMonthDay(&milliseconds, 1);
 	//Sep 23 2018 15:22:00
 	scTEqualMilli(__LINE__, 1537716120000, milliseconds);
 
 	//Sep 22 2018 15:22:00
 	milliseconds = 1537629720000;
-	scMAddMonthDay(&milliseconds, 30);
+	scMilliAddMonthDay(&milliseconds, 30);
 	//Oct 22 2018 15:22:00
 	scTEqualMilli(__LINE__, 1540221720000, milliseconds);
 
 	//Feb 22 2019 15:22:00
 	milliseconds = 1550848920000;
-	scMAddMonthDay(&milliseconds, 394);
+	scMilliAddMonthDay(&milliseconds, 394);
 	//Mar 22 2020 15:22:00
 	scTEqualMilli(__LINE__, 1584890520000, milliseconds);
 
 	//Sep 22 2018 15:22:00
 	milliseconds = 1537629720000;
-	scMAddMonthDay(&milliseconds, 60);
+	scMilliAddMonthDay(&milliseconds, 60);
 	//Nov 21 2018 15:22:00
 	scTEqualMilli(__LINE__, 1542813720000, milliseconds);
 
 	//Sep 22 2018 15:22:00
 	milliseconds = 1537629720000;
-	scMAddMonthDay(&milliseconds, -200);
+	scMilliAddMonthDay(&milliseconds, -200);
 	//Mar 06 2018 15:22:00
 	scTEqualMilli(__LINE__, 1520349720000, milliseconds);
 
 }
 
-void scMAddMonthTest(){
+void scMilliAddMonthTest(){
 	setNewTest(__func__, __FILE__);
 
 	//Sep 22 2018 15:22:00
 	Milliseconds milliseconds = 1537629720000;
-	scMAddMonth(&milliseconds, 12);
+	scMilliAddMonth(&milliseconds, 12);
 	//Sep 22 2019 15:22:00
 	scTEqualMilli(__LINE__, 1569165720000, milliseconds);
 
 	//Sep 22 2018 15:22:00
 	milliseconds = 1537629720000;
-	scMAddMonth(&milliseconds, 3);
+	scMilliAddMonth(&milliseconds, 3);
 	//Dec 22 2018 15:22:00
 	scTEqualMilli(__LINE__, 1545492120000, milliseconds);
 
 	//Sep 22 2018 15:22:00
 	milliseconds = 1537629720000;
-	scMAddMonth(&milliseconds, 16);
+	scMilliAddMonth(&milliseconds, 16);
 	//Jan 22 2020 15:22:00
 	scTEqualMilli(__LINE__, 1579706520000, milliseconds);
 
 	//Dec 22 2019 15:22:00
 	milliseconds = 1577028120000;
-	scMAddMonth(&milliseconds, 15);
+	scMilliAddMonth(&milliseconds, 15);
 	//Mar 22 2021 15:22:00
 	scTEqualMilli(__LINE__, 1616426520000, milliseconds);
 
 	//Dec 22 2019 15:22:00
 	milliseconds = 1577028120000;
-	scMAddMonth(&milliseconds, -15);
+	scMilliAddMonth(&milliseconds, -15);
 	//Sep 22 2018 15:22:00
 	scTEqualMilli(__LINE__, 1537629720000, milliseconds);
 
 }
 
-void scMAddYearTest(){
+void scMilliAddYearTest(){
 	setNewTest(__func__, __FILE__);
 
 	//Sep 22 2565 15:22:00
 	Milliseconds milliseconds = 18799312920000;
-	scMAddYear(&milliseconds, -435);
+	scMilliAddYear(&milliseconds, -435);
 	//Sep 22 2130 15:22:00
 	scTEqualMilli(__LINE__, 5071994520000, milliseconds);
 
 	//Sep 22 2018 15:22:00
 	milliseconds = 1537629720000;
-	scMAddYear(&milliseconds, 465);
+	scMilliAddYear(&milliseconds, 465);
 	//Sep 22 2483 15:22:00
 	scTEqualMilli(__LINE__, 16211632920000, milliseconds);
 
 	//Sep 22 2018 15:22:00
 	milliseconds = 1537629720000;
-	scMAddYear(&milliseconds, 4);
+	scMilliAddYear(&milliseconds, 4);
 	//Sep 22 2022 15:22:00
 	scTEqualMilli(__LINE__, 1663860120000, milliseconds);
 
@@ -203,164 +203,164 @@ void scMAddYearTest(){
 
 /* Sets */
 
-void scMSetMillisecondTest(){
+void scMilliSetMillisecondTest(){
 	setNewTest(__func__, __FILE__);
 
 	//Sep 22 2565 15:22:00 000
 	Milliseconds milliseconds = 18799312920000;
-	scMSetMillisecond(&milliseconds, 560);
+	scMilliSetMillisecond(&milliseconds, 560);
 	//Sep 22 2565 15:22:00 560
 	scTEqualMilli(__LINE__, 18799312920560, milliseconds);
 
-	scMSetMillisecond(&milliseconds, 987);
+	scMilliSetMillisecond(&milliseconds, 987);
 	//Sep 22 2565 15:22:00 987
 	scTEqualMilli(__LINE__, 18799312920987, milliseconds);
 
-	scMSetMillisecond(&milliseconds, 265);
+	scMilliSetMillisecond(&milliseconds, 265);
 	//Sep 22 2565 15:22:00 265
 	scTEqualMilli(__LINE__, 18799312920265, milliseconds);
 
-	scMSetMillisecond(&milliseconds, 156);
+	scMilliSetMillisecond(&milliseconds, 156);
 	//Sep 22 2565 15:22:00 156
 	scTEqualMilli(__LINE__, 18799312920156, milliseconds);
 
 }
 
-void scMSetSecondTest(){
+void scMilliSetSecondTest(){
 	setNewTest(__func__, __FILE__);
 
 	//Sep 22 2565 15:22:00
 	Milliseconds milliseconds = 18799312920000;
 
-	scMSetSecond(&milliseconds, 50);
+	scMilliSetSecond(&milliseconds, 50);
 	//Sep 22 2565 15:22:50
 	scTEqualMilli(__LINE__, 18799312970000, milliseconds);
 
-	scMSetSecond(&milliseconds, 30);
+	scMilliSetSecond(&milliseconds, 30);
 	//Sep 22 2565 15:22:50
 	scTEqualMilli(__LINE__, 18799312950000, milliseconds);
 
-	scMSetSecond(&milliseconds, 40);
+	scMilliSetSecond(&milliseconds, 40);
 	//Sep 22 2565 15:22:50
 	scTEqualMilli(__LINE__, 18799312960000, milliseconds);
 
 }
 
-void scMSetMinuteTest(){
+void scMilliSetMinuteTest(){
 	setNewTest(__func__, __FILE__);
 
 	//Sep 22 2565 15:22:00
 	Milliseconds milliseconds = 18799312920000;
 
-	scMSetMinute(&milliseconds, 50);
+	scMilliSetMinute(&milliseconds, 50);
 	//Sep 22 2565 15:50:00
 	scTEqualMilli(__LINE__, 18799314600000, milliseconds);
 
-	scMSetMinute(&milliseconds, 30);
+	scMilliSetMinute(&milliseconds, 30);
 	//Sep 22 2565 15:30:00
 	scTEqualMilli(__LINE__, 18799313400000, milliseconds);
 
-	scMSetMinute(&milliseconds, 40);
+	scMilliSetMinute(&milliseconds, 40);
 	//Sep 22 2565 15:40:00
 	scTEqualMilli(__LINE__, 18799314000000, milliseconds);
 
 }
 
-void scMSetHourTest(){
+void scMilliSetHourTest(){
 	setNewTest(__func__, __FILE__);
 
 	//Sep 22 2565 15:22:00
 	Milliseconds milliseconds = 18799312920000;
 
-	scMSetHour(&milliseconds, 16);
+	scMilliSetHour(&milliseconds, 16);
 	//Sep 22 2565 16:22:00
 	scTEqualMilli(__LINE__, 18799316520000, milliseconds);
 
-	scMSetHour(&milliseconds, 0);
+	scMilliSetHour(&milliseconds, 0);
 	//Sep 22 2565 00:22:00
 	scTEqualMilli(__LINE__, 18799258920000, milliseconds);
 
-	scMSetHour(&milliseconds, 23);
+	scMilliSetHour(&milliseconds, 23);
 	//Sep 22 2565 23:22:00
 	scTEqualMilli(__LINE__, 18799341720000, milliseconds);
 
 }
 
-void scMSetMonthDayTest(){
+void scMilliSetMonthDayTest(){
 	setNewTest(__func__, __FILE__);
 
 	//Sep 22 2019 15:22:00
 	Milliseconds milliseconds = 1569165720000;
 
-	scMSetMonthDay(&milliseconds, 16);
+	scMilliSetMonthDay(&milliseconds, 16);
 	//Sep 16 2019 15:22:00
 	scTEqualMilli(__LINE__, 1568647320000, milliseconds);
 
-	scMSetMonthDay(&milliseconds, 1);
+	scMilliSetMonthDay(&milliseconds, 1);
 	//Sep 01 2019 15:22:00
 	scTEqualMilli(__LINE__, 1567351320000, milliseconds);
 
-	scMSetMonthDay(&milliseconds, 30);
+	scMilliSetMonthDay(&milliseconds, 30);
 	//Sep 30 2019 15:22:00
 	scTEqualMilli(__LINE__, 1569856920000, milliseconds);
 
 }
 
-void scMSetMonthTest(){
+void scMilliSetMonthTest(){
 	setNewTest(__func__, __FILE__);
 
 	//Sep 22 2019 15:22:00
 	Milliseconds milliseconds = 1569165720000;
 
-	scMSetMonth(&milliseconds, JANUARY);
+	scMilliSetMonth(&milliseconds, JANUARY);
 	//Jan 22 2019 15:22:00
 	scTEqualMilli(__LINE__, 1548170520000, milliseconds);
 
-	scMSetMonth(&milliseconds, MARCH);
+	scMilliSetMonth(&milliseconds, MARCH);
 	//Mar 22 2019 15:22:00
 	scTEqualMilli(__LINE__, 1553268120000, milliseconds);
 
-	scMSetMonth(&milliseconds, DECEMBER);
+	scMilliSetMonth(&milliseconds, DECEMBER);
 	//Dec 22 2019 15:22:00
 	scTEqualMilli(__LINE__, 1577028120000, milliseconds);
 
 }
 
-void scMSetYearTest(){
+void scMilliSetYearTest(){
 	setNewTest(__func__, __FILE__);
 
 	//Sep 22 2019 15:22:00
 	Milliseconds milliseconds = 1569165720000;
 
-	scMSetYear(&milliseconds, 2018);
+	scMilliSetYear(&milliseconds, 2018);
 	//Sep 22 2018 15:22:00
 	scTEqualMilli(__LINE__, 1537629720000, milliseconds);
 
-	scMSetYear(&milliseconds, 2022);
+	scMilliSetYear(&milliseconds, 2022);
 	//Sep 22 2022 15:22:00
 	scTEqualMilli(__LINE__, 1663860120000, milliseconds);
 
-	scMSetYear(&milliseconds, 2965);
+	scMilliSetYear(&milliseconds, 2965);
 	//Sep 22 2965 15:22:00
 	scTEqualMilli(__LINE__, 31422093720000, milliseconds);
 
 }
 
-void scMJumpToWeekDayTest(){
+void scMilliJumpToWeekDayTest(){
 	setNewTest(__func__, __FILE__);
 
 	//Sep 22 2019 15:22:00
 	Milliseconds milliseconds = 1569165720000;
 
-	scMJumpToWeekDay(&milliseconds, THURSDAY);
+	scMilliJumpToWeekDay(&milliseconds, THURSDAY);
 	//Sep 26 2019 15:22:00
 	scTEqualMilli(__LINE__, 1569511320000, milliseconds);
 
-	scMJumpToWeekDay(&milliseconds, SATURDAY);
+	scMilliJumpToWeekDay(&milliseconds, SATURDAY);
 	//Sep 28 2019 15:22:00
 	scTEqualMilli(__LINE__, 1569684120000, milliseconds);
 
-	scMJumpToWeekDay(&milliseconds, SUNDAY);
+	scMilliJumpToWeekDay(&milliseconds, SUNDAY);
 	//Sep 29 2019 15:22:00 
 	scTEqualMilli(__LINE__, 1569770520000, milliseconds);
 
@@ -369,20 +369,20 @@ void scMJumpToWeekDayTest(){
 void scTestMilliseconds(){
 
 	//Adds
-	scMAddSecondTest();
-	scMAddMinuteTest();
-	scMAddHourTest();
-	scMAddMonthDayTest();
-	scMAddMonthTest();
+	scMilliAddSecondTest();
+	scMilliAddMinuteTest();
+	scMilliAddHourTest();
+	scMilliAddMonthDayTest();
+	scMilliAddMonthTest();
 
 	//Sets
-	scMAddYearTest();
-	scMSetMillisecondTest();
-	scMSetSecondTest();
-	scMSetMinuteTest();
-	scMSetHourTest();
-	scMSetMonthDayTest();
-	scMSetMonthTest();
-	scMSetYearTest();
-	scMJumpToWeekDayTest();
+	scMilliAddYearTest();
+	scMilliSetMillisecondTest();
+	scMilliSetSecondTest();
+	scMilliSetMinuteTest();
+	scMilliSetHourTest();
+	scMilliSetMonthDayTest();
+	scMilliSetMonthTest();
+	scMilliSetYearTest();
+	scMilliJumpToWeekDayTest();
 }
