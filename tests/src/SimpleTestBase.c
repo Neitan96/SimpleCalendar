8468932c5c7e4ddb79cfbf890df1676dc17caeb5
Nathan Almeida <nathanalmeidadev@outlook.com>
2019-08-15 22:24:41 -0300

    Adicionado marcado de tempo para os testes

--- tests/src/SimpleTestBase.c
+++ tests/src/SimpleTestBase.c
@@ -7,6 +7,7 @@
 
 #include <stdio.h>
 #include <stdarg.h>
+#include <time.h>
 #include "../../heads/SimpleCalendarBase.h"
 #include "../heads/SimpleMillisecondsTest.h"
 
@@ -17,6 +18,8 @@ int testNumberNow = 0;
 int subTestNumberNow = 0;
 int totalSubTest = 0;
 int totalFailsSubTest = 0;
+
+double executionTime;
 
 
 void scTError(int line, char* params){
@@ -34,8 +37,19 @@ void setNewTest(char* testName, char* file){
 }
 
 void scTFinalizeTests(){
-	printf("\nFinished tests: Total: %i, Total subs: %i, Sucess: %i, Fails: %i",
-			testNumberNow, totalSubTest, totalSubTest-totalFailsSubTest, totalFailsSubTest);
+	if(totalFailsSubTest < 1){
+		printf("\n\033[1;32m# Successful tests!!!\033[0m");
+	}else{
+		printf("\n\033[1;31m# Failed Testing!!!\033[0m");
+	}
+
+	printf("\n\033[1;35mResult tests\033[0m: ");
+	printf("\n        #Total tests: \033[1;36m%i\033[0m, ", testNumberNow);
+	printf("\n        #Total sub-tests: \033[01;33m%i\033[0m, ", totalSubTest);
+	printf("\n        #Sucess: \033[1;32m%i\033[0m, ", totalSubTest-totalFailsSubTest);
+	printf("\n        #Fails: \033[1;31m%i\033[0m", totalFailsSubTest);
+
+	printf("\nTests finished in \033[1;33m%g milliseconds\033[0m", executionTime);
 }
 
 void scTExecuteAllTests(){
@@ -54,7 +68,13 @@ void scTEqualMilli(int line, Milliseconds m1, Milliseconds m2){
 }
 
 int main() {
+    clock_t start = clock();
+
 	scTExecuteAllTests();
+
+	clock_t end = clock();
+	executionTime = (end - start) * 1000.0 / CLOCKS_PER_SEC;
+
 	scTFinalizeTests();
 	printf("\n");
 }
